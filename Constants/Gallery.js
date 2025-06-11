3f1f167b9136a3bb9a6efff140917ed88e896bee
Venus62341 <nakashopgtaacc@gmail.com>
2025-06-11 13:03:08 +0200

    Update Gallery.js
    Add some maps

--- Constants/Gallery.js
+++ Constants/Gallery.js
@@ -11,6 +11,8 @@ module.exports = Object.freeze({
     "#mp_rr_olympus_mu1_night": "olympus-after-dark",
 
     // Normal Map
+    "#mp_rr_canyonlands_dayzero": "kings-canyon",
+    "#mp_rr_district": "https://cdn.imgpile.com/f/i3iSqPK_xl.png",
     "#mp_rr_canyonlands": "kings-canyon",
     "#mp_rr_desertlands": "world-edge",
     "#mp_rr_olympus": "olympus",
@@ -18,24 +20,29 @@ module.exports = Object.freeze({
     "#mp_rr_divided_moon": "broken-moon",
 
     // Arena Map
+    "#mp_rr_thunderdome": "https://cdn.imgpile.com/f/A1zn8JD_xl.png",
     "#mp_rr_arena_phase_runner": "phase-runner",
     "#mp_rr_party_crasher": "party-crasher",
     "#mp_rr_aqueduct": "overflow",
     "#mp_rr_arena_skygarden": "encore",
     "#mp_rr_arena_composite": "drop-off",
     "#mp_rr_arena_habitat": "habitat",
-    "#mp_rr_freedm_map_name_the_core": "the-core",
+    "#mp_rr_freedm_map_name_the_core": "https://cdn.imgpile.com/f/IaiCS9X_xl.png",
 
     // Control Map
     "#control_map_name_lab": "hammond-labs",
     "#control_map_name_barometer": "barometer",
     "#control_map_name_caustic": "caustic-treatment",
-    "#control_map_name_production": "production",
+    "#control_map_name_hammond_labs_night": "hammond-labs-night",
+    "#control_map_name_lava_siphon": "lava-siphon",
+    "#control_map_name_production": "https://cdn.imgpile.com/f/Qpg2EBI_xl.png",
 
     // Free DM
     "#freedm_map_name_skulltown": "skull-town",
     "#freedm_map_name_estates": "estates",
-    "#freedm_map_name_dome": "dome",
-    "#freedm_map_name_wattson": "wattsons-pylon",
-    "#freedm_map_name_amps": "amps",
+    "#freedm_map_name_estates_night": "estates-night",
+    "#freedm_map_name_thewall": "the-wall",
+    "#freedm_map_name_fragment": "fragment-east",
+    "#freedm_map_name_fragment": "https://cdn.imgpile.com/f/slCKYfz_xl.png",
+    "#freedm_map_name_amps": "https://cdn.imgpile.com/f/XwVll5b_xl.png"
 });
