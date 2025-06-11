768d00ae6a645bc5f3e64edb0046fbca8c037f38
Venus62341 <nakashopgtaacc@gmail.com>
2025-06-11 13:05:24 +0200

    Update Translation.js
    Add some maps

--- Constants/Translation.js
+++ Constants/Translation.js
@@ -4,10 +4,16 @@ module.exports = Object.freeze({
     "#PL_FIRINGRANGE-PARTY": "Shooting Friends in the Firing Range",
 
     // Lobby
-    "#RICHPRESENCE_PARTY_LOBBY_N_N": "Chilling in the Lobby...",
-    "#RICHPRESENCE_PARTY_LOBBY": "Chilling in the Lobby...",
+    "#RICHPRESENCE_PARTY_LOBBY_N_N": "In the Lobby.",
+    "#RICHPRESENCE_PARTY_LOBBY": "In the Lobby",
 
     // Game Mode
+    "#PL_DAYZERO": "LAUNCH ROYALE",
+    "#GAMEMODE_OBJECTIVE_BR": "KNOCKOUT",
+    "#BATTLE_RUSH_REVIVAL_TITLE": "STRAIGHT SHOT-REVIVAL",
+    "#BOTPLAYGROUND_BOT_ROYALE_HEADER": "BOT ROYALE",
+    "#FREEDM_LOCKDOWN_MODE": "LOCKDOWN",
+    "#PL_QUAD": "QUADS",
     "#PL_TRIO": "TRIOS",
     "#PL_DUO": "DUOS",
     "#PL_Ranked_Leagues": "RANKED LEAGUES",
@@ -16,6 +22,7 @@ module.exports = Object.freeze({
     "#GAME_MODE_CONTROL": "CONTROL",
     "#GAME_MODE_GUNGAME": "GUN GAME",
     "#PL_RANKED_LEAGUES": "RANKED LEAGUES",
+    "#BTDM_NAME": "BIG TEAM DEATHMATCH",
     "#TDM_NAME": "TEAM DEATHMATCH",
     "#PL_DUO_DATE": "DATE NIGHT",
     "#PL_TRAINING": "In the training",
@@ -31,31 +38,37 @@ module.exports = Object.freeze({
     "#mp_rr_olympus_mu1_night": "After Dark",
 
     // Normal Map
+    "#mp_rr_canyonlands_dayzero": "Kings Canyon Classic",
+    "#mp_rr_district": "E-District",
     "#mp_rr_canyonlands": "Kings Canyon",
-    "#mp_rr_desertlands": "World Edge",
+    "#mp_rr_desertlands": "World's Edge",
     "#mp_rr_olympus": "Olympus",
     "#mp_rr_tropic_island": "Storm Point",
     "#mp_rr_divided_moon": "Broken Moon",
 
     // Arena Map
+    "#mp_rr_thunderdome": "Thunderdome",
     "#mp_rr_arena_phase_runner": "Phase Runner",
     "#mp_rr_party_crasher": "Party Crasher",
     "#mp_rr_aqueduct": "Overflow",
     "#mp_rr_arena_skygarden": "Encore",
     "#mp_rr_arena_composite": "Drop-Off",
     "#mp_rr_arena_habitat": "Habitat 4",
-    "#mp_rr_freedm_map_name_the_core": "The Core",
 
     // Control Map
     "#control_map_name_barometer": "Barometer",
     "#control_map_name_lab": "Hammond Labs",
     "#control_map_name_caustic": "Caustic Treatment",
+    "#control_map_name_hammond_labs_night": "Hammond Labs Night",
+    "#control_map_name_lava_siphon": "Lava Siphon",
     "#control_map_name_production": "Production Yard",
 
     // Free DM
+    "#freedm_map_name_fragment": "Fragment East",
     "#freedm_map_name_skulltown": "Skull Town",
     "#freedm_map_name_estates": "Estates",
-    "#freedm_map_name_dome": "Phase Runner",
-    "#freedm_map_name_wattson": "Wattson's Pylon",
-    "#freedm_map_name_amps": "ZEUS Station",
+    "#freedm_map_name_estates_night": "Estates Night",
+    "#freedm_map_name_thewall": "The Wall",
+    "#freedm_map_name_fragment": "Fragment East"
+
 });
